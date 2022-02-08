377070db4c0cf48bef72f0574bd4b237f617a308
ThaisSantiago <thaisrpm10@gmail.com>
2022-02-07 22:31:58 -0300

    atualização blog Pessoal

--- src/main/java/org/Generation/BlogPessoal/repository/UsuarioRepository.java
+++ src/main/java/org/Generation/BlogPessoal/repository/UsuarioRepository.java
@@ -7,10 +7,10 @@ import org.Generation.BlogPessoal.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-public Optional<Usuario> findByUsuario(String usuario);
-public List <Usuario> findAllByNomeContainingIgnoreCase(String nome); 
+	public Optional<Usuario> findByUsuario(String usuario);
+
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
 
 }
